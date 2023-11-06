1060227ebc4465c36566f33d48a998d33053eac1
XIEThunder <2111140047y@ed.fuk.kindai.ac.jp>
2023-11-06 11:14:44 +0000

    w

---
+++ L5/L5/app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  def create
+    p = BCrypt::Password.create(params[:user][:pass])
+    @user = User.new(uid: params[:user][:uid], pass: p, age: params[:user][:age])
+    @user.save
+    redirect_to users_path
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path
+  end
+end
